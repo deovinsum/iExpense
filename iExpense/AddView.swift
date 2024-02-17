f9a0c5002a4d9a8ad4d22a6a95f23f0e564b0b8c
deovinsum <gh@deovinsum.ru>
2024-02-18 02:02:27 +0300

    Initial Commit

---
+++ iExpense/AddView.swift
@@ -0,0 +1,45 @@
+// Created by deovinsum
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    let types = ["Business", "Personal"]
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let newItem = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(newItem)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,12 @@
+// Created by deovinsum
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
